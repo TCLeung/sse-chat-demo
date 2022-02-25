d7ad58e036783fd9b01f85be17fee157a643f2e3
Ken Leung <tcleungken@gmail.com>
2022-02-25 11:19:06 +0800

    Change avatar

--- web/src/components/Avatar.js
+++ web/src/components/Avatar.js
@@ -1,9 +1,10 @@
 import PropTypes from 'prop-types';
+import classNames from 'classnames'
 
 function Avatar(props) {
   const {name, className} = props;
-  const avatarUrl = `https://avatars.dicebear.com/api/bottts/${name.trim()}.svg`;
-  return <img className={className} alt={name} src={avatarUrl}/>;
+  const avatarUrl = `https://avatars.dicebear.com/api/pixel-art-neutral/${name.trim()}.svg`;
+  return <img className={classNames('rounded-full', className)} alt={name} src={avatarUrl}/>;
 }
 
 Avatar.propTypes = {
